47ca6a9d9d9f1d5d4150168bc18406c0d7b8ae9d
yves848 <yves.godart@gmail.com>
2024-04-24 21:12:34 +0200

    wip

--- go.mod
+++ go.mod
@@ -5,16 +5,14 @@ go 1.22.2
 require (
 	github.com/charmbracelet/bubbles v0.18.0
 	github.com/charmbracelet/bubbletea v0.25.0
-	github.com/charmbracelet/harmonica v0.2.0
 	github.com/charmbracelet/lipgloss v0.10.0
+	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54
 )
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
-	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
--- hello.go
+++ hello.go
@@ -1,17 +1,17 @@
-package main
+package hello
 
 import (
-	// "bufio"
+	"bufio"
 	"flag"
 	"fmt"
 
-	// "os"
+	"os"
 	"unicode/utf8"
 
 	tsize "github.com/kopoli/go-terminal-size"
 )
 
-func main() {
+func main2() {
 	type test struct {
 		name string
 		age  int
@@ -20,13 +20,22 @@ func main() {
 	var t []test
 	name := flag.String("name", "git", "name of the package")
 	flag.Parse()
-	t = make([]test, 3)
-	t[0] = test{"zhang", 12}
+	t = append(t, test{"zhang", 12})
+	t = append(t, test{"li", 13})
 	fmt.Println(t)
-	// scanner := bufio.NewScanner(os.Stdin)
-	// for scanner.Scan() {
-	// 	fmt.Println(scanner.Text())
-	// }
+	reader := bufio.NewReader(os.Stdin)
+	peekByte, err := reader.Peek(1)
+	fmt.Println(peekByte)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if len(peekByte) > 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}
 	fmt.Println(*name)
 	// cmd := exec.Command("winget", "search", "git")
 	// out, err := cmd.Output()
@@ -40,8 +49,8 @@ func main() {
 	fmt.Println(utf8.RuneCountInString(st))
 
 	var s tsize.Size
-	s, err := tsize.GetSize()
-	if err == nil {
+	s, err2 := tsize.GetSize()
+	if err2 == nil {
 		fmt.Println("Current Size is ", s.Width, "x", s.Height)
 	}
 }
